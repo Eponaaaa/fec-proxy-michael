2d6ec2001a8817b5134490ed6df9977930a13b29
Michael Johnson <mrj0730@icloud.com>
2020-03-28 15:14:05 -0500

    cors issues

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ const bodyparser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
-app.use(cors());
+app.use(express.static('public'));
+// app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(PORT, console.log(`Server listening on ${PORT}`));
+app.listen(PORT, () => console.log(`PROXY Server listening on ${PORT}`));
