ae5d9d9eff2036e92d6bb4ab57bb980719731298
Michael Johnson <mrj0730@icloud.com>
2020-03-28 15:16:48 -0500

    remove unneded module

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -8,7 +7,6 @@ const PORT = process.env.PORT || 4000;
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static('public'));
-// app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendStatus(200);
